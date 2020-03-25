9d188a7275c4df3b441d642f3b11665f44991bd9
Matei <stroiamatei@gmail.com>
2020-03-25 16:59:31 +0200

    Initial commit

---
+++ src/main/java/VVSS-Lab3MV/App.java
@@ -0,0 +1,13 @@
+package VVSS-Lab3MV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
